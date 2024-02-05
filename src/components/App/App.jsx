acd4338d3bad8abec20fb03ed9dd554abb2b4d9b
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-04 18:14:56 -0800

    feat: Give header basic styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,18 +2,20 @@ import './App.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Home from '../Home/Home.jsx'
-// import Navigation from '../Navigation/Navigation.jsx'
+import Navigation from '../Navigation/Navigation.jsx'
 import { Routes, Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="App">
       <header>
-        <Link to={'/'}><h1>Tavern Keeper</h1></Link>
-        {/* <Navigation /> */}
+        <Link className='home-button' to={'/'}>
+          <h1 className='app-title'>Tavern Keeper</h1>
+        </Link>
+        <Navigation />
       </header>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
         {/* <Route path="/login" element={<Login />}/>
         <Route path="/:id" element={<EncounterDetails />}/>
         <Route path="/builder" element={<EncounterBuilder />}/> */}
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,5 +2,15 @@ import PropTypes from 'prop-types';
 import './Navigation.css';
 import { NavLink } from 'react-router-dom';
 
+const Navigation = () => {
+  return (
+    <div className='Navigation'>
+      
+    </div>
+  );
+};
+
 Navigation.propTypes = {
 };
+
+export default Navigation;
