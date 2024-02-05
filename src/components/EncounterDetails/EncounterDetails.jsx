ce98ab75bc75f4739bcd05f3d4d76908d47f49bd
Arden Ranta <arranta20@gmail.com>
2024-02-04 23:43:37 -0800

    feat/add further styling and headers

--- src/components/EncounterDetails/EncounterDetails.jsx
+++ src/components/EncounterDetails/EncounterDetails.jsx
@@ -1,20 +1,24 @@
 import PropTypes from 'prop-types';
 import './EncounterDetails.css';
 
-export default function EncounterDetails({ name, party_size, party_level, summary, description, treasure, encounter_monsters }) {
+export default function EncounterDetails({ id, name, party_size, party_level, summary, description, treasure, encounter_monsters }) {
   const selectedMonsters = encounter_monsters.map((monster) => (
     <details>
       <summary>{monster.name}</summary>
+      <h3>Size:</h3>
       <p>{monster.size}</p>
+      <h3>Type:</h3>
       <p>{monster.type}</p>
+      <h3>Alignment:</h3>
       <p>{monster.alignment}</p>
+      <h3>Challenge Rating:</h3>
       <p>{monster.challenge_rating}</p>
     </details>
   ));
 
   return (
-    <section className='details'>
-      <section className='encounter-header'>
+    <section className='main-details'>
+      <section className='base-box encounter-header'>
         <h2 className='encounter-name'>{name}</h2>
         <div className='party-stats'>
           <h3>Party Size:</h3>
@@ -23,7 +27,7 @@ export default function EncounterDetails({ name, party_size, party_level, summar
           <p>{party_level}</p>
         </div>
       </section>
-      <section className='encounter-desc'>
+      <section className='base-box encounter-desc'>
         <h2>Encounter Description</h2>
         <h3>Short Summary:</h3>
         <p>{summary}</p>
@@ -32,8 +36,8 @@ export default function EncounterDetails({ name, party_size, party_level, summar
         <h3>Treasure and Rewards:</h3>
         <p>{treasure}</p>
       </section>
-      <section>
-        <h2>Your Critters</h2>
+      <section className='base-box encounter-foes'>
+        <h2>Your Enemies</h2>
         {selectedMonsters}
       </section>
     </section>
