3c9fa7f49a43fd77fd09552f952155930e97e298
Arden Ranta <arranta20@gmail.com>
2024-02-04 17:11:12 -0800

    fix/fix imports according to vercel error messaging

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import PropTypes from 'prop-types';
-import Navigation from '../Navigation/Navigation.jsx'
+// import Navigation from '../Navigation/Navigation.jsx'
 import { Routes, Route, Link } from 'react-router-dom';
 
 export default function App() {
