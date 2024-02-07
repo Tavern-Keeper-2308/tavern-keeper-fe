706c4fcb74ac07956df187b2c89e1e82979f00ca
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-06 18:47:11 -0800

    feat: Add greeting

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,9 @@ import './Login.css';
 const Login = () => {
   return (
     <div className='Login'>
-      
+      <h2 className='greeting'>
+        Welcome Traveler...
+      </h2>
     </div>
   );
 };
