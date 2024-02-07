f90a1367c98fd451969f05590735ca99c82e52a2
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-06 20:21:00 -0800

    feat: Add login buttons and container

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,6 +8,11 @@ const Login = () => {
       <h2 className='greeting'>
         Welcome Traveler...
       </h2>
+      <div className='login-container'>
+        <button className='login-button'>user1</button>
+        <button className='login-button'>user2</button>
+        <button className='login-button'>user3</button>
+      </div>
     </div>
   );
 };
