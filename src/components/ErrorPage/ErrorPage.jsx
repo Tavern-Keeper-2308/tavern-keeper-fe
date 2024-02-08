5904b06743a6053db1a6aa373d45e505ba4beadc
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-07 20:09:13 -0800

    feat: Add responsive styling for tablet breakpoint

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -7,6 +7,7 @@ const ErrorPage = () => {
     <div className="ErrorPage">
       <img src={errorImage} className='error-image' />
       <div className="error-message">
+        {/* When we have error messages returned from BE, this copy will be replaced */}
         Uh oh! You seem to have landed in an unknown realm. <br /><br /> Please Plane Shift your way back Home and try again.
       </div>
     </div>
