af60a157634a91bba7ff64edcd23940099314ef4
Arden Ranta <arranta20@gmail.com>
2024-02-08 14:06:32 -0800

    feat/update mock data and begin html skeleton

--- src/components/EncounterBuilder/EncounterBuilder.jsx
+++ src/components/EncounterBuilder/EncounterBuilder.jsx
@@ -1,10 +1,32 @@
 import PropTypes from 'prop-types';
 import './EncounterBuilder.css';
+import React, { useEffect, useState } from 'react';
+import { monsters } from '../../mockDataset';
 
 const EncounterBuilder = () => {
+  const [monsterList, setMonsterList] = useState([]);
+
+  useEffect(() => {
+    //placeholder for fetch call/query to get all monsters
+
+
+  })
+
   return (
     <div className='EncounterBuilder'>
-      Encounter Builder Placeholder
+      <div>
+        <select>
+          <option value=""></option>
+        </select>
+        
+      </div>
+      <section className='encounter-header'>
+        <input type="text" aria-label="encounter name input" placeholder="Encounter Name" required></input>
+        <div className='party-stats'>
+          <input type="text" aria-label="party size input" placeholder="Party Size" required></input>
+          <input type="text" aria-label="party level input" placeholder="Party Level" required></input>
+        </div>
+      </section>
     </div>
   );
 };
