13f6bc80fc0d5de2b8e560f393a7435eae8d0905
Arden Ranta <arranta20@gmail.com>
2024-02-08 15:30:28 -0800

    feat/add remaining base html skeleton

--- src/components/EncounterBuilder/EncounterBuilder.jsx
+++ src/components/EncounterBuilder/EncounterBuilder.jsx
@@ -13,18 +13,41 @@ const EncounterBuilder = () => {
   })
 
   return (
-    <div className='EncounterBuilder'>
-      <div>
-        <select>
-          <option value=""></option>
-        </select>
-        
-      </div>
-      <section className='encounter-header'>
-        <input type="text" aria-label="encounter name input" placeholder="Encounter Name" required></input>
-        <div className='party-stats'>
-          <input type="text" aria-label="party size input" placeholder="Party Size" required></input>
-          <input type="text" aria-label="party level input" placeholder="Party Level" required></input>
+    <div className='main-details'>
+
+      <section className='base-box'>
+        <section className='encounter-header'>
+          <h3>Encounter Name:</h3>
+          <input type="text" aria-label="encounter name input" placeholder="" required></input>
+          <div className='party-stats'>
+            <h3>Party Size:</h3>
+            <input type="text" aria-label="party size input" placeholder="" required></input>
+            <h3>Party Level:</h3>
+            <input type="text" aria-label="party level input" placeholder="" required></input>
+          </div>
+        </section>
+        <section className='encounter-desc'>
+          <h3>Short Summary:</h3>
+          <input type="text" aria-label="summary input" placeholder="" required></input>
+          <h3>Encounter Description:</h3>
+          <input type="text" aria-label="description input" placeholder="" required></input>
+          <h3>Treasure and Rewards:</h3>
+          <input type="text" aria-label="loot input" placeholder="" required></input>
+        </section>
+      </section>
+      <section className='encounter-foes base-box'>
+        <div className='monster-selection'>
+          <select>
+            <option value="size">{}</option>
+          </select>
+          <select>
+            <option value="name">{}</option>
+          </select>
+          <input type="text" aria-label="hit points search" placeholder="Hit Points"></input>
+          <input type="text" aria-label="armor class search" placeholder="Armor Class"></input>
+        </div>
+        <div className='monster-selection'>
+          {}
         </div>
       </section>
     </div>
