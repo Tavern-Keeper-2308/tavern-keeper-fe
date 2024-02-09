75dce28ff47c1dff500a0634e75a48ebc934dd70
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-08 21:37:45 -0800

    feat: Add query for getMonster

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,39 +13,105 @@ import { allEncountersForUser } from '../../mockDataset.js';
 
 export default function App() {
   const [encounters, setEncounters] = useState([]);
+  const [monster, setMonster] = useState(null);
+
   const client = useApolloClient();
 
+  const getEncounters = async () => {
+    try {
+      const { data } = await client.query({
+        query: gql`
+          query getEncounters($userName: String!) {
+            encounters(userName: $userName) {
+                id
+                userName
+                encounterName
+                partySize
+                partyLevel
+                summary
+                description
+                treasure
+                encounterMonsters {
+                    monsterName
+                }
+            }
+        }
+  `,
+        variables: {
+          "userName": "demo-many-encounters"
+        },
+      });
+      setEncounters(data.encounters);
+    } catch (error) {
+      console.error('Error fetching encounters: ', error);
+    }
+  };
+
+  const getMonster = async () => {
+    try {
+      const { data } = await client.query({
+        query: gql`
+          query getMonster($index: String!) {
+            monster(index: $index) {
+                monsterIndex
+                monsterName
+                size
+                type
+                armorClass
+                speed {
+                    walk
+                    fly
+                    swim
+                }
+                hitPoints
+                strength
+                dexterity
+                constitution
+                intelligence
+                wisdom
+                charisma
+                damageVulnerabilities
+                damageResistances
+                damageImmunities
+                conditionImmunities
+                proficiencyBonus
+                proficiencies {
+                    name
+                    value
+                }
+                senses {
+                    blindsight
+                    darkvision
+                    passivePerception
+                }
+                specialAbilities {
+                    name
+                    desc
+                }
+                actions {
+                    name
+                    desc
+                }
+                legendaryActions {
+                    name
+                    desc
+                }
+            }
+        }
+        `,
+        variables: {
+          "index": "aboleth"
+        },
+      });
+      setMonster(data.monster);
+    } catch (error) {
+      console.error('Error fetching monster: ', error);
+    }
+  };
+
   useEffect(() => {
-    const getEncounters = async () => {
-      try {
-        const { data } = await client.query({
-          query: gql`
-            query getEncounters($userName: String!) {
-              encounters(userName: $userName) {
-                  id
-                  userName
-                  encounterName
-                  partySize
-                  partyLevel
-                  summary
-                  description
-                  treasure
-                  encounterMonsters {
-                      monsterName
-                  }
-              }
-          }
-    `,
-          variables: {
-            "userName": "demo-many-encounters"
-          },
-        });
-        setEncounters(data.encounters);
-      } catch (error) {
-        console.error('Error fetching encounters: ', error);
-      }
-    };
     getEncounters();
+    // getMonster();
   }, [client]);
 
   return (
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css';
 import EncounterPreviewContainer from '../EncounterPreviewContainer/EncounterPreviewContainer';
 
 export default function Home({ encounters }) {
+  // console.log(monster);
+
   return (
     <div className='Home'>
       <EncounterPreviewContainer encounters={encounters} />
