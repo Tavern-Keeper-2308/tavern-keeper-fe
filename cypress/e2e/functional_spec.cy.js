1509f7900e7af56ec640d7a79628bed757738407
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-10 12:57:49 -0800

    test: Add test for rendering elements on login page

--- cypress/e2e/functional_spec.cy.js
+++ cypress/e2e/functional_spec.cy.js
@@ -13,4 +13,14 @@ context('Functional Tests', () => {
       aliasMutation(req, 'createEncounter')
     });
   });
+
+  it('should render elements correctly on login page', () => {
+    cy.visit('/login');
+    cy.get('.app-title').contains('Tavern Keeper');
+    cy.get('.greeting').contains('Welcome Traveler...');
+    cy.get('.login-container').children().should('have.length', 3);
+    cy.get('.login-button').first().contains('DEMO - No Encounters');
+    cy.get('.login-button').eq(1).contains('DEMO - One Encounter');
+    cy.get('.login-button').last().contains('DEMO - Many Encounters');
+  });
 });
