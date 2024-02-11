e147d237c2e9afcf589edeabad707c416aa4cc0e
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-10 19:46:52 -0800

    test: Complete tests for first encounter detail monster

--- cypress/e2e/functional_spec.cy.js
+++ cypress/e2e/functional_spec.cy.js
@@ -30,10 +30,10 @@ context('Functional Tests', () => {
       // Check if it's a getEncounter query
       if (hasOperationName(req, 'getEncounter')) {
         // Check scenario and set appropriate fixture
-        if (req.body.variables.id === 13) {
+        if (req.body.variables.id === '13') {
           req.alias = 'gqlGetEncounterQuery';
           req.reply({ fixture: 'mock_data_getEncounter.json' });
-        } 
+        }
       }
 
       // Check if it's a getMonsters query
@@ -42,16 +42,19 @@ context('Functional Tests', () => {
         if (req) {
           req.alias = 'gqlGetMonstersQuery';
           req.reply({ fixture: 'mock_data_getMonsters.json' });
-        } 
+        }
       }
 
       // Check if it's a getMonster query
       if (hasOperationName(req, 'getMonster')) {
         // Check scenario and set appropriate fixture
         if (req.body.variables.index === 'ancient-red-dragon') {
-          req.alias = 'gqlGetMonsterQuery';
-          req.reply({ fixture: 'mock_data_getMonster.json' });
-        } 
+          req.alias = 'gqlGetMonsterQueryAncientRedDragon';
+          req.reply({ fixture: 'mock_data_getMonster_ancient_red_dragon.json' });
+        } else if (req.body.variables.index === 'kobold') {
+          req.alias = 'gqlGetMonsterQueryKobold';
+          req.reply({ fixture: 'mock_data_getMonster_kobold.json' });
+        }
       }
     });
   });
@@ -191,6 +194,132 @@ context('Functional Tests', () => {
 
 
   ///////* Detail Page *///////
+  it('should render expected elements correctly on the encounter details page', () => {
+    cy.visit('/login');
+    cy.get('.login-button').contains('DEMO - Many Encounters').click();
+    cy.wait('@gqlGetEncountersQueryManyEncounters')
+      .its('response.body.data.encounters')
+      .should((encounters) => {
+        expect(encounters.length).to.equal(7);
+      });
 
+    cy.get('.preview-card-link').eq(4).click();
+    cy.wait('@gqlGetEncounterQuery')
+      .its('response.body.data.encounter')
+      .should('exist')
+      .and('be.an', 'object');
+
+    cy.wait('@gqlGetMonsterQueryKobold')
+      .its('response.body.data.monster')
+      .should('exist')
+      .and('be.an', 'object');
+
+    cy.wait('@gqlGetMonsterQueryAncientRedDragon')
+      .its('response.body.data.monster')
+      .should('exist')
+      .and('be.an', 'object');
+
+    cy.get('.encounter-name').contains("We each need to find our own inspiration, Kiki. Sometimes it's not easy.");
+
+    cy.get('.party-stats').within(() => {
+      cy.get('.party-size-heading').contains('Party Size:');
+      cy.get('.party-size-value').contains('2');
+      cy.get('.party-level-heading').contains('Party Level');
+      cy.get('.party-level-value').contains('12');
+    });
+
+    cy.get('.encounter-desc').within(() => {
+      cy.get('.section-heading').contains('Short Summary:');
+      cy.get('.short-summary-value').contains('The world was young, the mountains green');
+      cy.get('.section-heading').contains('Encounter Description:');
+      cy.get('.encounter-description-value').contains('Hideous gibbous decadent noisome manuscript blasphemous. Gibbering furtive stench loathsome fungus. Iridescence abnormal tenebrous. Shunned antediluvian gibbering. Stench cat tenebrous gambrel.');
+      cy.get('.section-heading').contains('Treasure and Rewards:');
+      cy.get('.treasure-value').contains("Ahzidal's Ring of Arcana");
+    });
+
+    cy.get('.encounter-foes').within(() => {
+      cy.get('.enemies-heading').contains('Your Enemies');
+      cy.get('.monster-details').should('have.length', 3);
+
+      cy.get('.monster-details').first().within(() => {
+        cy.get('.monster-name').contains('Kobold');
+
+        cy.get('.foe-physical').within(() => {
+          cy.get('.section-heading').contains('Size:');
+          cy.get('.size-value').contains('SMALL');
+          cy.get('.section-heading').contains('Armor Class:');
+          cy.get('.ac-value').contains('12');
+          cy.get('.section-heading').contains('Hit Points:');
+          cy.get('.hp-value').contains('5');
+        });
+
+        cy.get('.foe-stats').within(() => {
+          cy.get('.section-heading').contains('Strength');
+          cy.get('.strength-value').contains('7');
+          cy.get('.section-heading').contains('Dexterity');
+          cy.get('.dexterity-value').contains('15');
+          cy.get('.section-heading').contains('Constitution');
+          cy.get('.constitution-value').contains('9');
+          cy.get('.section-heading').contains('Intelligence');
+          cy.get('.intelligence-value').contains('8');
+          cy.get('.section-heading').contains('Wisdom');
+          cy.get('.wisdom-value').contains('7');
+          cy.get('.section-heading').contains('Charisma');
+          cy.get('.charisma-value').contains('8');
+          cy.get('.section-heading').contains('Proficiency Bonus');
+          cy.get('.pb-value').contains('2');
+        });
+
+        cy.get('.foe-speeds').within(() => {
+          cy.get('.section-heading').contains('Walk Speed:');
+          cy.get('.walk-speed-value').contains('30 ft.');
+          cy.get('.section-heading').contains('Swim Speed:');
+          cy.get('.swim-speed-value').should('be.empty');
+          cy.get('.section-heading').contains('Fly Speed:');
+          cy.get('.fly-speed-value').should('be.empty');
+          cy.get('.section-heading').contains('Burrow Speed:');
+          cy.get('.burrow-speed-value').should('be.empty');
+        });
+
+        cy.get('.damage-mods').within(() => {
+          cy.get('.section-heading').contains('Damage Vulnerabilities:');
+          cy.get('.dmg-vulnerability-value').contains('none');
+          cy.get('.section-heading').contains('Damage Resistances:');
+          cy.get('.dmg-resistance-value').contains('none');
+          cy.get('.section-heading').contains('Damage Immunities:');
+          cy.get('.dmg-immunity-value').contains('none');
+          cy.get('.section-heading').contains('Condition Immunities:');
+          cy.get('.condition-immunity-value').contains('none');
+        });
+
+        cy.get('.senses').within(() => {
+          cy.get('.section-heading').contains('Blindsight:');
+          cy.get('.blindsight-value').should('be.empty');
+          cy.get('.section-heading').contains('Darkvision:');
+          cy.get('.darkvision-value').contains('60 ft.');
+          cy.get('.section-heading').contains('Passive Perception:');
+          cy.get('.passive-perception-value').contains('8');
+        });
+
+        cy.get('.special-abilities').within(() => {
+          cy.get('.section-heading').contains('Special Ability: Sunlight Sensitivity');
+          cy.get('.special-ability-value').contains('While in sunlight, the kobold has disadvantage on attack rolls, as well as on Wisdom (Perception) checks that rely on sight.');
+          cy.get('.section-heading').contains('Special Ability: Pack Tactics');
+          cy.get('.special-ability-value').contains("The kobold has advantage on an attack roll against a creature if at least one of the kobold's allies is within 5 ft. of the creature and the ally isn't incapacitated.");
+        });
+
+        cy.get('.standard-actions').within(() => {
+          cy.get('.section-heading').contains('Action: Dagger');
+          cy.get('.standard-action-value').contains("Melee Weapon Attack: +4 to hit, reach 5 ft., one target. Hit: 4 (1d4 + 2) piercing damage.");
+          cy.get('.section-heading').contains('Action: Sling');
+          cy.get('.standard-action-value').contains("Ranged Weapon Attack: +4 to hit, range 30/120 ft., one target. Hit: 4 (1d4 + 2) bludgeoning damage.");
+        });
+
+      })
+
+    })
+
+
+  });
 
 });
--- src/components/EncounterDetails/EncounterDetails.jsx
+++ src/components/EncounterDetails/EncounterDetails.jsx
@@ -95,146 +95,146 @@ export default function EncounterDetails({ selectedEncounter }) {
         const monsters = monsterDetailsArray.map((monster) => {
 
           const damageVulnerabilitiesArray = monster.details.damageVulnerabilities.map((vulnerability) => (
-            <div key={vulnerability}>
-              <p>{vulnerability}</p>
+            <div className='section-heading' key={vulnerability}>
+              <p className='dmg-vulnerability-value'>{vulnerability}</p>
             </div>
           ));
 
           const damageResistancesArray = monster.details.damageResistances.map((resistance) => (
-            <div key={resistance}>
-              <p>{resistance}</p>
+            <div className='section-heading' key={resistance}>
+              <p className='dmg-resistance-value'>{resistance}</p>
             </div>
           ));
 
           const damageImmunitiesArray = monster.details.damageImmunities.map((immunity) => (
-            <div key={immunity}>
-              <p>{immunity}</p>
+            <div className='section-heading' key={immunity}>
+              <p className='dmg-immunity-value'>{immunity}</p>
             </div>
           ));
 
           const conditionImmunitiesArray = monster.details.damageImmunities.map((immunity) => (
-            <div key={immunity}>
-              <p>{immunity}</p>
+            <div className='section-heading' key={immunity}>
+              <p className='condition-immunity-value'>{immunity}</p>
             </div>
           ));
 
           const proficienciesArray = monster.details.proficiencies && monster.details.proficiencies.map((prof) => (
             <div className='black foe-proficiencies' key={prof?.name}>
-              <h3>{prof?.name}</h3>
-              <p>{prof?.value}</p>
+              <h3 className='section-heading'>{prof?.name}</h3>
+              <p className='proficiency-value'>{prof?.value}</p>
             </div>
           ));
 
           const specialAbilitiesArray = monster.details.specialAbilities && monster.details.specialAbilities.map((ability) => (
             <div className='black foe-special-abilities' key={ability?.name}>
-              <h3>Special Ability: {ability?.name}</h3>
-              <p>{ability?.desc}</p>
+              <h3 className='section-heading'>Special Ability: {ability?.name}</h3>
+              <p className='special-ability-value'>{ability?.desc}</p>
             </div>
           ));
 
           const standardActionsArray = monster.details.actions && monster.details.actions.map((action) => (
             <div className='black foe-standard-actions' key={action?.name}>
-              <h3>Action: {action?.name}</h3>
-              <p>{action?.desc}</p>
+              <h3 className='section-heading'>Action: {action?.name}</h3>
+              <p className='standard-action-value'>{action?.desc}</p>
             </div>
           ));
 
           const legendaryActionsArray = monster.details.legendaryActions && monster.details.legendaryActions.map((action) => (
             <div className='black foe-legendary-actions' key={action?.name}>
-              <h3>Legendary Action: {action?.name}</h3>
-              <p>{action?.desc}</p>
+              <h3 className='section-heading'>Legendary Action: {action?.name}</h3>
+              <p className='legendary-action-value'>{action?.desc}</p>
             </div>
           ));
 
           return (
-            <details key={uuid()} index={monster.monsterIndex}>
-              <summary>{monster.details.monsterName}</summary>
+            <details className='monster-details' key={uuid()} index={monster.monsterIndex}>
+              <summary className='monster-name'>{monster.details.monsterName}</summary>
               <section className='black foe-physical'>
-                <h3>Size:</h3>
-                <p>{monster.details && monster.details.size}</p>
-                <h3>Armor Class:</h3>
-                <p>{monster.details && monster.details.armorClass}</p>
-                <h3>Hit Points:</h3>
-                <p>{monster.details && monster.details.hitPoints}</p>
+                <h3 className='section-heading'>Size:</h3>
+                <p className='size-value'>{monster.details && monster.details.size}</p>
+                <h3 className='section-heading'>Armor Class:</h3>
+                <p className='ac-value'>{monster.details && monster.details.armorClass}</p>
+                <h3 className='section-heading'>Hit Points:</h3>
+                <p className='hp-value'>{monster.details && monster.details.hitPoints}</p>
               </section>
               <section className='black foe-stats'>
                 <div className='attributes'>
-                  <h3>Strength</h3>
-                  <p>{monster.details && monster.details.strength}</p>
+                  <h3 className='section-heading'>Strength</h3>
+                  <p className='strength-value'>{monster.details && monster.details.strength}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Dexterity</h3>
-                  <p>{monster.details && monster.details.dexterity}</p>
+                  <h3 className='section-heading'>Dexterity</h3>
+                  <p className='dexterity-value'>{monster.details && monster.details.dexterity}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Constitution</h3>
-                  <p>{monster.details && monster.details.constitution}</p>
+                  <h3 className='section-heading'>Constitution</h3>
+                  <p className='constitution-value'>{monster.details && monster.details.constitution}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Intelligence</h3>
-                  <p>{monster.details && monster.details.intelligence}</p>
+                  <h3 className='section-heading'>Intelligence</h3>
+                  <p className='intelligence-value'>{monster.details && monster.details.intelligence}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Wisdom</h3>
-                  <p>{monster.details && monster.details.wisdom}</p>
+                  <h3 className='section-heading'>Wisdom</h3>
+                  <p className='wisdom-value'>{monster.details && monster.details.wisdom}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Charisma</h3>
-                  <p>{monster.details && monster.details.charisma}</p>
+                  <h3 className='section-heading'>Charisma</h3>
+                  <p className='charisma-value'>{monster.details && monster.details.charisma}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Proficiency Bonus</h3>
-                  <p>{monster.details && monster.details.proficiencyBonus}</p>
+                  <h3 className='section-heading'>Proficiency Bonus</h3>
+                  <p className='pb-value'>{monster.details && monster.details.proficiencyBonus}</p>
                 </div>
               </section>
               <section className='black foe-speeds'>
                 <div className='attributes'>
-                  <h3>Walk Speed:</h3>
-                  <p>{monster.details && monster.details.speed.walk}</p>
+                  <h3 className='section-heading'>Walk Speed:</h3>
+                  <p className='walk-speed-value'>{monster.details && monster.details.speed.walk}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Swim Speed:</h3>
-                  <p>{monster.details && monster.details.speed.swim}</p>
+                  <h3 className='section-heading'>Swim Speed:</h3>
+                  <p className='swim-speed-value'>{monster.details && monster.details.speed.swim}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Fly Speed:</h3>
-                  <p>{monster.details && monster.details.speed.fly}</p>
+                  <h3 className='section-heading'>Fly Speed:</h3>
+                  <p className='fly-speed-value'>{monster.details && monster.details.speed.fly}</p>
                 </div>
                 <div className='attributes'>
-                  <h3>Burrow Speed:</h3>
-                  <p>{monster.details && monster.details.speed.burrow}</p>
+                  <h3 className='section-heading'>Burrow Speed:</h3>
+                  <p className='burrow-speed-value'>{monster.details && monster.details.speed.burrow}</p>
                 </div>
               </section>
               <section className='black damage-mods'>
                 <div>
-                  <h3>Damage Vulnerabilities:</h3>
-                  {monster.details.damageVulnerabilities.length === 0 ? <p>none</p> : damageVulnerabilitiesArray}
+                  <h3 className='section-heading'>Damage Vulnerabilities:</h3>
+                  {monster.details.damageVulnerabilities.length === 0 ? <p className='dmg-vulnerability-value'>none</p> : damageVulnerabilitiesArray}
                 </div>
                 <div>
-                  <h3>Damage Resistances:</h3>
-                  {monster.details.damageResistances.length === 0 ? <p>none</p> : damageResistancesArray}
+                  <h3 className='section-heading'>Damage Resistances:</h3>
+                  {monster.details.damageResistances.length === 0 ? <p className='dmg-resistance-value'>none</p> : damageResistancesArray}
                 </div>
                 <div>
-                  <h3>Damage Immunities:</h3>
-                  {monster.details.damageImmunities.length === 0 ? <p>none</p> : damageImmunitiesArray}
+                  <h3 className='section-heading'>Damage Immunities:</h3>
+                  {monster.details.damageImmunities.length === 0 ? <p className='dmg-immunity-value'>none</p> : damageImmunitiesArray}
                 </div>
                 <div>
-                  <h3>Condition Immunities:</h3>
-                  {monster.details.conditionImmunities.length === 0 ? <p>none</p> : conditionImmunitiesArray}
+                  <h3 className='section-heading'>Condition Immunities:</h3>
+                  {monster.details.conditionImmunities.length === 0 ? <p className='condition-immunity-value'>none</p> : conditionImmunitiesArray}
                 </div>
               </section>
               <section className='black senses'>
                 <div>
-                  <h3>Blindsight:</h3>
-                  <p>{monster.details && monster.details.senses.blindsight}</p>
+                  <h3 className='section-heading'>Blindsight:</h3>
+                  <p className='blindsight-value'>{monster.details && monster.details.senses.blindsight}</p>
                 </div>
                 <div>
-                  <h3>Darkvision:</h3>
-                  <p>{monster.details && monster.details.senses.darkvision}</p>
+                  <h3 className='section-heading'>Darkvision:</h3>
+                  <p className='darkvision-value'>{monster.details && monster.details.senses.darkvision}</p>
                 </div>
                 <div>
-                  <h3>Passive Perception:</h3>
-                  <p>{monster.details && monster.details.senses.passive_perception}</p>
+                  <h3 className='section-heading'>Passive Perception:</h3>
+                  <p className='passive-perception-value'>{monster.details && monster.details.senses.passivePerception}</p>
                 </div>
               </section>
               <section className='proficiencies'>{proficienciesArray}</section>
@@ -260,23 +260,23 @@ export default function EncounterDetails({ selectedEncounter }) {
             <section className='encounter-header'>
               <h2 className='encounter-name'>{selectedEncounter.encounterName}</h2>
               <div className='party-stats'>
-                <h3>Party Size:</h3>
-                <p>{selectedEncounter.partySize}</p>
-                <h3>Party Level:</h3>
-                <p>{selectedEncounter && selectedEncounter.partyLevel}</p>
+                <h3 className='party-size-heading'>Party Size:</h3>
+                <p className='party-size-value'>{selectedEncounter.partySize}</p>
+                <h3 className='party-level-heading'>Party Level:</h3>
+                <p className='party-level-value'>{selectedEncounter && selectedEncounter.partyLevel}</p>
               </div>
             </section>
             <section className='encounter-desc'>
-              <h3>Short Summary:</h3>
-              <p>{selectedEncounter.summary}</p>
-              <h3>Encounter Description:</h3>
-              <p>{selectedEncounter.description}</p>
-              <h3>Treasure and Rewards:</h3>
-              <p>{selectedEncounter.treasure}</p>
+              <h3 className='section-heading'>Short Summary:</h3>
+              <p className='short-summary-value'>{selectedEncounter.summary}</p>
+              <h3 className='section-heading'>Encounter Description:</h3>
+              <p className='encounter-description-value'>{selectedEncounter.description}</p>
+              <h3 className='section-heading'>Treasure and Rewards:</h3>
+              <p className='treasure-value'>{selectedEncounter.treasure}</p>
             </section>
           </section>
           <section className='base-box encounter-foes'>
-            <h2>Your Enemies</h2>
+            <h2 className='enemies-heading'>Your Enemies</h2>
             {loading ? (
               <p className='loading-message'>Loading...</p>
             ) : (
