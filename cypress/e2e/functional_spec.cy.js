2f60a53fb373caba799c2c70cd41161f5bdc64ba
Erica Hagle <ericahagle.dev@gmail.com>
2024-02-10 16:28:44 -0800

    test: Add tests for user with one encounter home rendering

--- cypress/e2e/functional_spec.cy.js
+++ cypress/e2e/functional_spec.cy.js
@@ -14,8 +14,17 @@ context('Functional Tests', () => {
 
       // Check if it's a getEncounters query
       if (hasOperationName(req, 'getEncounters')) {
-        req.alias = 'gqlGetEncountersQuery';
-        req.reply({ fixture: 'mock_data_getEncounters_none.json' });
+        // Check scenario and set appropriate fixture
+        if (req.body.variables.userName === 'demo-no-encounters') {
+          req.alias = 'gqlGetEncountersQueryNoEncounters';
+          req.reply({ fixture: 'mock_data_getEncounters_none.json' });
+        } else if (req.body.variables.userName === 'demo-one-encounter') {
+          req.alias = 'gqlGetEncountersQueryOneEncounter';
+          req.reply({ fixture: 'mock_data_getEncounters_one.json' });
+        } else {
+          req.alias = 'gqlGetEncountersQueryManyEncounters';
+          req.reply({ fixture: 'mock_data_getEncounters_many.json' });
+        }
       }
     });
   });
@@ -33,7 +42,7 @@ context('Functional Tests', () => {
   it('should render header elements correctly on home page when logging in', () => {
     cy.visit('/login');
     cy.get('.login-button').contains('DEMO - No Encounters').click();
-    cy.wait('@gqlGetEncountersQuery')
+    cy.wait('@gqlGetEncountersQueryNoEncounters')
       .its('response.body.data.encounters')
       .should((encounters) => {
         expect(encounters.length).to.equal(0);
@@ -49,15 +58,56 @@ context('Functional Tests', () => {
   it('should render expected elements correctly on home page when logging in as DEMO - No Encounters', () => {
     cy.visit('/login');
     cy.get('.login-button').contains('DEMO - No Encounters').click();
-    cy.wait('@gqlGetEncountersQuery')
+    cy.wait('@gqlGetEncountersQueryNoEncounters')
       .its('response.body.data.encounters')
       .should((encounters) => {
         expect(encounters.length).to.equal(0);
       });
   });
 
-  it.skip('should render expected elements correctly on home page when logging in as DEMO - One Encounter', () => {
-
+  it('should render expected elements correctly on home page when logging in as DEMO - One Encounter', () => {
+    cy.visit('/login');
+    cy.get('.login-button').contains('DEMO - One Encounter').click();
+    cy.wait('@gqlGetEncountersQueryOneEncounter')
+      .its('response.body.data.encounters')
+      .should((encounters) => {
+        expect(encounters.length).to.equal(1);
+      });
+    cy.get('.EncounterPreviewContainer').children().should('have.length', 1);
+    cy.get('.EncounterPreviewCard').first().within(() => {
+      cy.get('.preview-encounter-name').contains("We each need to find our own inspiration, Kiki. Sometimes it's not easy.");
+      cy.get('.preview-section-title').contains('Encounter Summary');
+      cy.get('.preview-encounter-summary').contains('The world was young, the mountains green');
+      cy.get('.preview-section-title').contains('Monster(s)');
+      cy.get('.preview-monster-list').children().first().contains('Kobold');
+      cy.get('.preview-monster-list').children().eq(1).contains('Kobold');
+      cy.get('.preview-monster-list').children().last().contains('Ancient Red Dragon');
+      cy.get('.preview-party-size').within(() => {
+        cy.get('.preview-section-title').contains('Party Size');
+        cy.get('.preview-party-size-value').contains('2');
+      });
+      cy.get('.preview-party-level').within(() => {
+        cy.get('.preview-section-title').contains('Party Level');
+        cy.get('.preview-party-level-value').contains('12');
+      });
+    });
+    cy.get('.EncounterPreviewCard').last().within(() => {
+      cy.get('.preview-encounter-name').contains("We each need to find our own inspiration, Kiki. Sometimes it's not easy.");
+      cy.get('.preview-section-title').contains('Encounter Summary');
+      cy.get('.preview-encounter-summary').contains('The world was young, the mountains green');
+      cy.get('.preview-section-title').contains('Monster(s)');
+      cy.get('.preview-monster-list').children().first().contains('Kobold');
+      cy.get('.preview-monster-list').children().eq(1).contains('Kobold');
+      cy.get('.preview-monster-list').children().last().contains('Ancient Red Dragon');
+      cy.get('.preview-party-size').within(() => {
+        cy.get('.preview-section-title').contains('Party Size');
+        cy.get('.preview-party-size-value').contains('2');
+      });
+      cy.get('.preview-party-level').within(() => {
+        cy.get('.preview-section-title').contains('Party Level');
+        cy.get('.preview-party-level-value').contains('12');
+      });
+    });
   });
 
   it.skip('should render expected elements correctly on home page when logging in as DEMO - Many Encounters', () => {
